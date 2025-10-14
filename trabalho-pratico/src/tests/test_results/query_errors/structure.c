13233e1abd14184756d0266f0cb99b5533b1c140
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-14 13:58:24 +0000

    feat: initialize scaffolding for tests (structure.c)

---
+++ trabalho-pratico/src/tests/test_results/query_errors/structure.c
@@ -0,0 +1,7 @@
+#include "tests/test_results/query_errors.h"
+
+// Estrutura de armazenamento da entidade erro da query
+typedef struct storage_query_errors {
+    GPtrArray *data;
+} STORAGE_QUERY_ERRORS, *StorageQueryErrors;
+
