9206be7fb707599310df739e3479c628ec15e77b
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-14 21:02:52 +0000

    feat: initialize scaffolding for interactive mode (interactive_utils.c)

---
+++ trabalho-pratico/src/interactive_program/interactive_utils.c
@@ -0,0 +1,8 @@
+#include "interactive_program/interactive_utils.h"
+
+#include "parsers/parser_main.h"
+#include "queries/queries.h"
+#include "interactive_program/graphics.h"
+
+#include <stdlib.h>
+#include <string.h>
