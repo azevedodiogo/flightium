3ebf140db6840581f61c27f6bbb027f7e76aa0ee
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-15 16:01:52 +0000

    refactor: clean up and simplify codebase in parser_airports.h

--- trabalho-pratico/include/parsers/parser_airports.h
+++ trabalho-pratico/include/parsers/parser_airports.h
@@ -6,3 +6,10 @@
 // {- Parser -}
 
 /**
+ * @brief Carrega e valida uma linha do dataset dos aeroportos.
+ * @param input Linha lida do ficheiro CSV.
+ * @param database Base de dados onde o registo será inserido.
+ * @return 0 em caso de sucesso. Caso contrário, devolve-se um valor não nulo se ocorrer erro de parsing.
+ */
+int load_line_airports (char *input, Database database);
+
