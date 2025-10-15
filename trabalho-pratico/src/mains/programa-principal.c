eeac5bf403af8b65a940ef4990b76b03674dd0e9
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-15 20:26:01 +0000

    feat: initialize scaffolding for project files (programa-principal.c)

---
+++ trabalho-pratico/src/mains/programa-principal.c
@@ -0,0 +1,7 @@
+#include "managers/database.h"
+#include "parsers/parser_main.h"
+#include "queries/queries.h"
+#include "utils/utility_functions.h"
+
+// Função principal
+int main (int argc, char **argv) {
