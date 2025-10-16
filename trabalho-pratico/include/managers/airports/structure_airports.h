532e7279ce6ddcbed080a717c524883c1f241bed
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-16 18:14:29 +0000

    feat: initialize scaffolding for airports manager (structure_airports.h)

---
+++ trabalho-pratico/include/managers/airports/structure_airports.h
@@ -0,0 +1,7 @@
+#ifndef STRUCTURE_AIRPORTS_H
+#define STRUCTURE_AIRPORTS_H
+
+#include "utils/date_functions.h"
+
+/** @brief Estrutura de armazenamento dos aeroportos. */
+typedef struct storage_airports *StorageAirports;
