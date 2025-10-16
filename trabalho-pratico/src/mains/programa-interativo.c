f157f993c0cb18629f49e11fe666d4fbbee539d9
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-16 13:28:05 +0000

    feat: initialize scaffolding for project files (programa-interativo.c)

---
+++ trabalho-pratico/src/mains/programa-interativo.c
@@ -0,0 +1,7 @@
+#include "interactive_program/input_queries.h"
+#include "interactive_program/interactive_utils.h"
+#include "interactive_program/graphics.h"
+
+#include <stdlib.h>
+
+#define BUFFER 1024
