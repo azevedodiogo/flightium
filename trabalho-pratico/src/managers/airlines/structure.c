6e5c862d0ef345271635e6e9b187bd34b33fab83
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-16 21:38:56 +0000

    feat: initialize scaffolding for airlines manager (structure.c)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/src/managers/airlines/structure.c
@@ -0,0 +1,7 @@
+#include "managers/airlines/structure_airlines.h"
+#include "managers/airlines/entity_airline.h"
+#include "managers/storage_generic.h"
+
+// Estrutura de armazenamento das companhias aéreas
+typedef struct storage_airlines {
+    StorageGeneric data;
