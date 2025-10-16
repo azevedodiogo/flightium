c436502c33ad8990caf60371c618e6fcb8802803
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-16 16:15:53 +0000

    feat: improve validation and handle bounds in general_validations.c
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/src/parsers/general_validations.c
+++ trabalho-pratico/src/parsers/general_validations.c
@@ -4,3 +4,11 @@
 #include <glib.h>
 
 #define NO_VALUE -1
+
+// Verifica se uma string representa corretamente um código
+int validate_code (const char *string) {
+    return isupper (string [0]) && isupper (string [1]) && isupper (string [2]) && string [3] == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+// Verifica se uma string representa corretamente um número de documento
+int validate_document_number (const char *string) {
