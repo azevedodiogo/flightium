adaf15212c0545c759615b93ab069d315a40246b
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-16 10:34:48 +0000

    feat: initialize scaffolding for query 3 (query3.c)

---
+++ trabalho-pratico/src/queries/query3.c
@@ -0,0 +1,7 @@
+#include "queries/queries.h"
+
+#include "utils/date_functions.h"
+#include "managers/airports/entity_airport.h"
+#include "managers/airports/structure_airports.h"
+
+#define NO_VALUE -1
