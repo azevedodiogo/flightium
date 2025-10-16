2552900bc2dcacf8523dfaf54bd8f0ee1c121246
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-16 18:50:57 +0000

    feat: initialize scaffolding for tests (structure.c)

---
+++ trabalho-pratico/src/tests/test_results/query_results/structure.c
@@ -0,0 +1,8 @@
+#include "tests/test_results/query_results.h"
+
+#include <glib.h>
+
+#define NUMBER_QUERIES 6
+
+// Estrutura de armazenamento da entidade resultado de query
+typedef struct storage_query_results {
