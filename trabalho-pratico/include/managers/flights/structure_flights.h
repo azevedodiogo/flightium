52f324dc7149bbed0a56d003b33462fb54213b74
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-17 18:07:36 +0000

    feat: initialize scaffolding for flights manager (structure_flights.h)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/include/managers/flights/structure_flights.h
@@ -0,0 +1,7 @@
+#ifndef STRUCTURE_FLIGHTS_H
+#define STRUCTURE_FLIGHTS_H
+
+#include "utils/date_functions.h"
+
+/** @brief Estrutura de armazenamento dos voos. */
+typedef struct storage_flights *StorageFlights;
