07f17b2e44ac38fc951f118e967d96790996c0df
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-17 20:38:32 +0000

    feat: initialize scaffolding for passengers manager (structure_passengers.h)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/include/managers/passengers/structure_passengers.h
@@ -0,0 +1,7 @@
+#ifndef STRUCTURE_PASSENGERS_H
+#define STRUCTURE_PASSENGERS_H
+
+#include "utils/date_functions.h"
+
+/** @brief Estrutura de armazenamento dos passageiros. */
+typedef struct storage_passengers *StoragePassengers;
