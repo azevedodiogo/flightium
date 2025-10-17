474178283e89ec9a479113253e884ffeb3be7fa8
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-17 19:07:40 +0000

    feat: initialize scaffolding for interactive mode (interactive_utils.h)

---
+++ trabalho-pratico/include/interactive_program/interactive_utils.h
@@ -0,0 +1,7 @@
+#ifndef INTERACTIVE_UTILS_H
+#define INTERACTIVE_UTILS_H
+
+#include "managers/database.h"
+
+/**
+ * @brief Prepara a base de dados a partir de um caminho fornecido no modo interativo.
