105bb54fc543f2778b8b1efbe18462ef66e90381
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-17 14:25:44 +0000

    refactor: optimize structures in parser_flights.h for project files

--- trabalho-pratico/include/parsers/parser_flights.h
+++ trabalho-pratico/include/parsers/parser_flights.h
@@ -5,3 +5,11 @@
 #include "managers/database.h"
 
 // {- Parser -}
+
+/**
+ * @brief Carrega e valida uma linha do dataset dos voos.
+ * @param input Linha lida do ficheiro CSV.
+ * @param database Base de dados onde o registo será inserido.
+ * @return 0 em caso de sucesso. Caso contrário, devolve-se um valor não nulo se ocorrer erro de parsing.
+ */
+int load_line_flights (char *input, Database database);
