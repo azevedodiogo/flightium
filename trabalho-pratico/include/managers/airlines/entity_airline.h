972e48f87c454d6f7b3c2daaa4b7a6ac3b01d387
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-18 19:19:15 +0000

    refactor: optimize structures in entity_airline.h for airlines manager

--- trabalho-pratico/include/managers/airlines/entity_airline.h
+++ trabalho-pratico/include/managers/airlines/entity_airline.h
@@ -5,3 +5,11 @@
 
 /** @brief Entidade que representa uma companhia aérea. */
 typedef struct entity_airline *EntityAirline;
+
+
+
+/**
+ * @brief Cria um registo de companhia aérea.
+ * @param name Nome da companhia.
+ * @param delay Valor inicial de atraso (em minutos).
+ * @return Nova entidade.
