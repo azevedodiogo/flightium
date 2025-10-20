7d27372be28111281d9b171f33d59dcb3f950f8c
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-20 17:40:40 +0000

    feat: initialize scaffolding for utils (utility_functions.h)

---
+++ trabalho-pratico/include/utils/utility_functions.h
@@ -0,0 +1,8 @@
+#ifndef UTILITY_FUNCTIONS_H
+#define UTILITY_FUNCTIONS_H
+
+#include <glib.h>
+
+/**
+ * @brief Separa uma string até encontrar o carácter indicado.
+ * @param str Ponteiro para a string a processar; é atualizado para o segmento restante.
