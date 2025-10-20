45e57c12300a37200f2b2da27fc5fa368f953bde
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-20 21:33:21 +0000

    feat: initialize scaffolding for tests (tests_parsers.c)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/src/tests/tests_parsers.c
@@ -0,0 +1,7 @@
+#include "tests/tests_parsers.h"
+
+#include "parsers/parser_main.h"
+#include "managers/database.h"
+#include "tests/test_results/test_results.h"
+#include "tests/test_results/load_timer.h"
+#include "tests/tests_utils.h"
