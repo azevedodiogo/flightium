d80248e6c5913bea4d115572aba58cf7e2141c6b
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-20 22:56:48 +0000

    refactor: optimize structures in entity.c for tests

--- trabalho-pratico/src/tests/test_results/query_errors/entity.c
+++ trabalho-pratico/src/tests/test_results/query_errors/entity.c
@@ -4,3 +4,12 @@
 
 #define NO_VALUE -1
 #define BUFFER 1024
+
+// Estrutura da entidade erro da query
+typedef struct query_error {
+    int query_num;
+    int command_num;
+    int error_line;
+    char expected_line [BUFFER];   
+    char actual_line [BUFFER];    
+} QUERY_ERROR, *QueryError;
