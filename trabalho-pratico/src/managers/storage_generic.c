e845c8de089aace92cbfcc2ce3a44ced916f3b22
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-20 13:48:16 +0000

    refactor: clean up and simplify codebase in storage_generic.c

--- trabalho-pratico/src/managers/storage_generic.c
+++ trabalho-pratico/src/managers/storage_generic.c
@@ -5,3 +5,10 @@ typedef struct storage_generic {
     gpointer *array;
     int len, capacity;
     GHashTable *indexing;
+    void (*destroy_func) (gpointer);
+} STORAGE_GENERIC, *StorageGeneric;
+
+
+
+// Cria uma estrutura de armazenamento genérica
+StorageGeneric create_storage_generic (int number_elements, void (*destroy_func) (gpointer), int flag) {
