68ebc2f79855729eeca74572c16b5afe669b7f64
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-20 15:01:56 +0000

    style: format code structure and variables in structure.c

--- trabalho-pratico/src/tests/test_results/query_results/structure.c
+++ trabalho-pratico/src/tests/test_results/query_results/structure.c
@@ -6,3 +6,11 @@
 
 // Estrutura de armazenamento da entidade resultado de query
 typedef struct storage_query_results {
+    QueryResult results [NUMBER_QUERIES];
+    char *expected_results;
+} STORAGE_QUERY_RESULTS, *StorageQueryResults;
+
+
+
+// Cria a estrutura de armazenamento da entidade resultado de query
+StorageQueryResults create_storage_query_results (char *expected_results) {
