465975c41f378b7d8d94b5b71687e887457833fd
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-21 15:41:24 +0000

    feat: initialize scaffolding for project files (parser_definitions.h)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/include/parsers/parser_definitions.h
@@ -0,0 +1,5 @@
+#ifndef PARSER_DEFINITIONS_H
+#define PARSER_DEFINITIONS_H
+
+/** @brief Estrutura opaca da base de dados para ocultar a implementação e evitar dependências circulares. */
+typedef struct database *Database;
