89cdc8c5b9f9df689e379d9d0edf23fe3b2d1b94
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-21 12:11:36 +0000

    feat: initialize scaffolding for passengers manager (entity.c)

---
+++ trabalho-pratico/src/managers/passengers/entity.c
@@ -0,0 +1,7 @@
+#include "managers/passengers/entity_passenger.h"
+
+// Entidade do passageiro
+typedef struct entity_passenger {
+    char *first_name;
+    char *last_name;
+    char *nationality;
