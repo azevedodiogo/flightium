7b16813ab051d1d099566c819d57754bbf7debe7
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-21 16:58:09 +0000

    feat: improve validation and handle bounds in validations.c

--- trabalho-pratico/src/parsers/reservations/validations.c
+++ trabalho-pratico/src/parsers/reservations/validations.c
@@ -4,3 +4,9 @@
 #include <glib.h>
 
 #define NO_VALUE -1
+
+// Verifica se uma string representa corretamente o id de uma reserva
+int validate_reservation_id (const char *string) {
+
+    // Verifica se o primeiro caractere não é um R
+    if (string [0] != 'R') return NO_VALUE;
