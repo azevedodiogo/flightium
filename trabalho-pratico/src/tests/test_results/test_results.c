bb3766483a2755f03953566c51c986d0466b5a7c
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-21 13:45:57 +0000

    feat: initialize scaffolding for tests (test_results.c)

---
+++ trabalho-pratico/src/tests/test_results/test_results.c
@@ -0,0 +1,8 @@
+#include "tests/test_results/test_results.h"
+
+#include "tests/test_results/load_timer.h"
+#include "tests/test_results/query_errors.h"
+#include "tests/test_results/query_results.h"
+
+// Estrutura de armazenamento dos testes
+typedef struct tests {
