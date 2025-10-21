fe983569922eff282d261105cc71f78b8c19bb66
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-21 20:58:13 +0000

    docs: add detailed comments and docstrings in entity_passenger.h

--- trabalho-pratico/include/managers/passengers/entity_passenger.h
+++ trabalho-pratico/include/managers/passengers/entity_passenger.h
@@ -6,3 +6,11 @@
 #include <glib.h>
 
 /** @brief Entidade que representa um passageiro. */
+typedef struct entity_passenger *EntityPassenger;   // Protege-se assim a estrutura interna, ou seja, o utilizador não a vê diretamente.
+
+
+/**
+ * @brief Cria um registo com a informação do passageiro.
+ * @param first_name Primeiro nome.
+ * @param last_name Último nome.
+ * @param nationality Nacionalidade.
