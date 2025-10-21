e73fa53c31d151d5b97c1d6edeef0c76cee4d387
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-21 19:36:33 +0000

    feat: initialize scaffolding for airports manager (structure.c)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/src/managers/airports/structure.c
@@ -0,0 +1,8 @@
+#include "managers/airports/structure_airports.h"
+
+#include "managers/airports/entity_airport.h"
+#include "managers/storage_generic.h"
+#include "utils/date_functions.h"
+#include "utils/utility_functions.h"
+
+#define N_EXPECTED_NATIONALITIES 55
