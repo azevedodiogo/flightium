4abe7e926d98cd620d8a81869f04a28906f292ca
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-21 16:35:06 +0000

    style: format code structure and variables in structure.c

--- trabalho-pratico/src/tests/test_results/query_errors/structure.c
+++ trabalho-pratico/src/tests/test_results/query_errors/structure.c
@@ -5,3 +5,12 @@ typedef struct storage_query_errors {
     GPtrArray *data;
 } STORAGE_QUERY_ERRORS, *StorageQueryErrors;
 
+
+
+// Cria a estrutura de armazenamento da entidade erro da query
+StorageQueryErrors create_storage_query_errors () {
+
+    // Aloca memória para a estrutura
+    StorageQueryErrors storage_query_errors = g_malloc (sizeof (STORAGE_QUERY_ERRORS));
+
+    // Cria a sub-estrutura
