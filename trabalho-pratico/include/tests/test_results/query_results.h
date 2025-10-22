b6011bddade21109f97100c5f00e82a6818a3eef
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-22 18:49:36 +0000

    feat: initialize scaffolding for tests (query_results.h)

---
+++ trabalho-pratico/include/tests/test_results/query_results.h
@@ -0,0 +1,8 @@
+#ifndef QUERY_RESULTS_H
+#define QUERY_RESULTS_H
+
+/** @brief Estrutura da entidade resultado da query. */
+typedef struct query_result *QueryResult;
+
+/** @brief Estrutura de armazenamento da entidade resultado de query. */
+typedef struct storage_query_results *StorageQueryResults;
