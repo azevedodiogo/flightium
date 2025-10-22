46a938b260ede4dfa669a0070ac5df46121f7c07
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-22 14:15:03 +0000

    feat: initialize scaffolding for query 2 (query2.c)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/src/queries/query2.c
@@ -0,0 +1,7 @@
+#include "queries/queries.h"
+
+#include "managers/aircrafts/entity_aircraft.h"
+#include "managers/aircrafts/structure_aircrafts.h"
+#include "managers/storage_generic.h"
+
+#define LARGE_BUFFER 4096
