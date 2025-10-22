c14041bbfcc6b0d0436b3b31edc397d41305825e
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-22 10:10:51 +0000

    feat: initialize scaffolding for database core (database.c)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/src/managers/database.c
@@ -0,0 +1,8 @@
+#include "managers/database.h"
+
+#include "managers/aircrafts/structure_aircrafts.h"
+#include "managers/airlines/structure_airlines.h"
+#include "managers/airports/structure_airports.h"
+#include "managers/flights/structure_flights.h"
+#include "managers/passengers/structure_passengers.h"
+#include "managers/reservations/structure_reservations.h"
