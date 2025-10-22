5dbf15ffb108b6d9b871e1d4ef5912a7f19d86aa
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-22 21:13:51 +0000

    test: update test cases and check bounds in structure_airlines.h
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

--- trabalho-pratico/include/managers/airlines/structure_airlines.h
+++ trabalho-pratico/include/managers/airlines/structure_airlines.h
@@ -6,3 +6,11 @@ typedef struct storage_airlines *StorageAirlines;
 
 /** @brief Entidade da companhia aérea. */
 typedef struct entity_airline *EntityAirline;
+
+
+
+/**
+ * @brief Cria a estrutura de armazenamento das companhias aéreas.
+ * @param inicial_capacity Capacidade inicial pretendida.
+ * @return Estrutura inicializada.
+ */
