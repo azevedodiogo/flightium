7c68d2a32ab728d3337e3c05ebf0cd1f319becaf
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-23 17:51:57 +0000

    feat: initialize scaffolding for query 5 (query5.c)

---
+++ trabalho-pratico/src/queries/query5.c
@@ -0,0 +1,7 @@
+#include "queries/queries.h"
+
+#include "managers/airlines/entity_airline.h"
+#include "managers/airlines/structure_airlines.h"
+#include "managers/storage_generic.h"
+
+#define LARGE_BUFFER 4096
