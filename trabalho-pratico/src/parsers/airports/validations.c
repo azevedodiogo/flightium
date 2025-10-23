82f40c1204feb494fe9f3ae8a2b87de4213a414e
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-23 19:28:30 +0000

    feat: improve validation and handle bounds in validations.c

--- trabalho-pratico/src/parsers/airports/validations.c
+++ trabalho-pratico/src/parsers/airports/validations.c
@@ -5,3 +5,12 @@
 
 // Verifica se uma string representa corretamente uma latitude
 int validate_latitude (const char *string) {
+
+    // Verifica se é negativo
+    if (string [0] == '-') string++;
+
+    // Verifica se o primeiro caractere não é um dígito
+    if (!isdigit (string [0])) return EXIT_FAILURE;
+
+    // Verifica se o formato não é válido (antes do ponto)
+    int i; for (i = 1; string [i] && string [i] != '.'; i++) if (i == 2 || !isdigit (string [i])) return EXIT_FAILURE;
