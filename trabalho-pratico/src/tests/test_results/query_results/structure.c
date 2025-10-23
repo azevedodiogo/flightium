1a0dff0d8296ca65ae7fece3fb21ac744c0eb883
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-23 11:25:43 +0000

    test: update test cases and check bounds in structure.c

--- trabalho-pratico/src/tests/test_results/query_results/structure.c
+++ trabalho-pratico/src/tests/test_results/query_results/structure.c
@@ -14,3 +14,10 @@ typedef struct storage_query_results {
 
 // Cria a estrutura de armazenamento da entidade resultado de query
 StorageQueryResults create_storage_query_results (char *expected_results) {
+
+    // Aloca memória para a estrutura
+    StorageQueryResults storage_query_results = g_malloc0 (sizeof (STORAGE_QUERY_RESULTS));
+
+    // Define as componentes da estrutura
+    for (int i = 0; i < NUMBER_QUERIES; i++) storage_query_results -> results [i] = create_query_result ();
+    storage_query_results -> expected_results = g_strdup (expected_results);
