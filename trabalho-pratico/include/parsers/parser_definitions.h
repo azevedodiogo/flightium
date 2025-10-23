5858e77effd7d7d72e67c7caadb9ede7ce041712
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-23 15:46:53 +0000

    test: update test cases and check bounds in parser_definitions.h
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/include/parsers/parser_definitions.h
+++ trabalho-pratico/include/parsers/parser_definitions.h
@@ -3,3 +3,10 @@
 
 /** @brief Estrutura opaca da base de dados para ocultar a implementação e evitar dependências circulares. */
 typedef struct database *Database;
+
+/** @brief Ponteiros para funções de carregamento de datasets. */
+int load_line_aircrafts (char *, Database);
+int load_line_airports (char *, Database);
+int load_line_passengers (char *, Database);
+int load_line_flights (char *, Database);
+int load_line_reservations (char *, Database);
