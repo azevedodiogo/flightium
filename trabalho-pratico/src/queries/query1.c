9a08901e1938e3deb1b8b826b8f567b412f773a5
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-24 11:05:00 +0000

    refactor: optimize structures in query1.c for query 1
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

--- trabalho-pratico/src/queries/query1.c
+++ trabalho-pratico/src/queries/query1.c
@@ -4,3 +4,11 @@
 #include "managers/airports/structure_airports.h"
 
 #define LARGE_BUFFER 4096
+
+// Executa a query 1
+void execute_query1 (Database database, char *input, char *output, char del) {
+
+    // Procura o aeroporto na estrutura de armazenamento
+    const struct entity_airport *airport = get_airport_by_id (get_database_airports (database), input);
+
+    // Verifica se o aeroporto existe
