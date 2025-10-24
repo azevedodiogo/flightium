86bc2b364fbe9a962809e349763ff2cf090619db
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-24 17:14:36 +0000

    refactor: clean up and simplify codebase in execute_queries.c

--- trabalho-pratico/src/queries/execute_queries.c
+++ trabalho-pratico/src/queries/execute_queries.c
@@ -4,3 +4,12 @@
 
 #define BUFFER 1024
 #define LARGE_BUFFER 4096
+
+// Abre o ficheiro do input
+int open_input_file (FILE **read_file, const char *read_file_name) {
+
+    // Abre o ficheiro
+    *read_file = fopen (read_file_name, "r");
+
+    // Verifica se o ficheiro não existe
+    if (*read_file == NULL) {
