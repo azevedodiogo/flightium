7470f7af3f286d8050e746e5aa986f7a0ef870a0
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-25 19:06:17 +0000

    feat: complete implementation of aircraft parser in validations.c

--- trabalho-pratico/src/parsers/aircrafts/validations.c
+++ trabalho-pratico/src/parsers/aircrafts/validations.c
@@ -5,3 +5,10 @@
 
 // Verifica se uma string representa corretamente um ano
 int validate_year (const char *string) {
+    
+    // Verifica se o formato é válido
+    if (!isdigit (string [0]) || !isdigit (string [1]) || !isdigit (string [2]) || !isdigit (string [3]) || string [4]) return EXIT_FAILURE;
+
+    // Verifica se o ano é válido
+    return atoi (string) < 2026 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
