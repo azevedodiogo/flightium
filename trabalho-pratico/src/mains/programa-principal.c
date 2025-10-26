e5d72fe69b1f4b988a515c05c6641a361100701a
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-26 12:09:43 +0000

    feat: improve validation and handle bounds in programa-principal.c

--- trabalho-pratico/src/mains/programa-principal.c
+++ trabalho-pratico/src/mains/programa-principal.c
@@ -13,3 +13,10 @@ int main (int argc, char **argv) {
     } int dataset;
 
     // Determina qual é o tipo de dataset
+    if (determine_dataset (argv [1], &dataset)) {
+        fprintf (stderr, "Error: invalid directory\n");
+        return EXIT_FAILURE;
+    }
+
+    // Cria a base de dados
+    Database database = create_database (dataset);
