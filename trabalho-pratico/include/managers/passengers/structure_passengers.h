7aaacc8699aefc30dcf198bf13be7ae32c0a53fd
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-26 18:21:24 +0000

    feat: implement core logic in structure_passengers.h for passengers manager

--- trabalho-pratico/include/managers/passengers/structure_passengers.h
+++ trabalho-pratico/include/managers/passengers/structure_passengers.h
@@ -5,3 +5,11 @@
 
 /** @brief Estrutura de armazenamento dos passageiros. */
 typedef struct storage_passengers *StoragePassengers;
+
+/** @brief Entidade do passageiro. */
+typedef struct entity_passenger *EntityPassenger;
+
+
+// Há abstração das estruturas internas através de typedefs para ponteiros.
+// Desta maneira, se houvesse necessidade de alterar alguma estrutura para outra, bastava mudar o ficheiro structure.c.
+// O resto do programa continuaria a chamar a função get_passenger, sem saber que por trás aconteceu a troca de uma Hash Table para um Array. 
