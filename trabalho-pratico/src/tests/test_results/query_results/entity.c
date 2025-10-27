30c2ad9139b840fb587a4a31f10e63b3b136d394
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-27 18:10:04 +0000

    refactor: optimize structures in entity.c for tests

--- trabalho-pratico/src/tests/test_results/query_results/entity.c
+++ trabalho-pratico/src/tests/test_results/query_results/entity.c
@@ -5,3 +5,11 @@
 // Estrutura da entidade resultado da query
 typedef struct query_result {
     int number_correct;
+    int number_executed;
+    double total_time;
+} QUERY_RESULT, *QueryResult;
+
+
+
+// Cria um registo da entidade resultado da query
+QueryResult create_query_result () {
