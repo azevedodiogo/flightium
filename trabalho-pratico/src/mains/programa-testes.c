6993f744d376dabce76b8d52059074e885b43a82
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-27 16:50:16 +0000

    feat: initialize scaffolding for project files (programa-testes.c)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/src/mains/programa-testes.c
@@ -0,0 +1,8 @@
+#define _POSIX_C_SOURCE 199309L
+
+#include "queries/queries.h"
+#include "utils/utility_functions.h"
+#include "tests/test_results/test_results.h"
+#include "tests/tests_parsers.h"
+#include "tests/tests_queries.h"
+#include "tests/tests_utils.h"
