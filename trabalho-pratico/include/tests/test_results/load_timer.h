04fb2c225c22c93b459efadff2dc1f38e82a2af5
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-27 19:16:06 +0000

    feat: initialize scaffolding for tests (load_timer.h)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/include/tests/test_results/load_timer.h
@@ -0,0 +1,7 @@
+#ifndef LOAD_TIMER_H
+#define LOAD_TIMER_H
+
+#include <glib.h>
+
+/** @brief Estrutura usada para medir tempos de carregamento de datasets. */
+typedef struct load_timer *LoadTimer;
