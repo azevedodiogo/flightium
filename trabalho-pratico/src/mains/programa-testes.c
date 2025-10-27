c393c675d4848a5377d2399e44df48e31fc535e8
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-27 21:45:52 +0000

    docs: add detailed comments and docstrings in programa-testes.c

--- trabalho-pratico/src/mains/programa-testes.c
+++ trabalho-pratico/src/mains/programa-testes.c
@@ -14,3 +14,11 @@
 int main (int argc, char **argv) {
 
     // Declara variáveis de teste
+    struct timespec start_time, end_time, prepare_start, prepare_end; struct rusage r_usage;
+
+    // Inicia os testes
+    clock_gettime (CLOCK_REALTIME, &start_time);
+
+    // Verifica se não foram dados três argumentos
+    if (argc != 4) {
+        fprintf (stderr, "Error: There should be three arguments\n");
