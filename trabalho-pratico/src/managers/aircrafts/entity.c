1fec4d89588dc7d661acc9e3233abb49cd3c5643
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-27 13:34:18 +0000

    refactor: optimize structures in entity.c for aircrafts manager

--- trabalho-pratico/src/managers/aircrafts/entity.c
+++ trabalho-pratico/src/managers/aircrafts/entity.c
@@ -5,3 +5,10 @@ typedef struct entity_aircraft {
     char *identifier;
     char *manufacturer;
     char *model;
+    unsigned short flight_count;
+} ENTITY_AIRCRAFT, *EntityAircraft;
+
+
+
+// Cria um registo da entidade aeronave
+EntityAircraft create_aircraft (const char *identifier, const char *manufacturer, const char *model) {
