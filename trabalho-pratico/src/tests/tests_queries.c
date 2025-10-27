301bdd70822dc0562f1b391de8dfd7531fdbf70a
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-27 19:36:16 +0000

    feat: initialize scaffolding for queries engine (tests_queries.c)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/src/tests/tests_queries.c
@@ -0,0 +1,8 @@
+#define _POSIX_C_SOURCE 199309L
+
+#include "tests/tests_queries.h"
+
+#include "tests/tests_utils.h"
+#include "tests/test_results/test_results.h"
+#include "managers/database.h"
+#include "queries/queries.h"
