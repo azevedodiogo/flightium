24c7971b8302954bd9759cc86d762b368cf11c42
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-28 20:58:03 +0000

    refactor: optimize structures in parser.c for aircraft parser
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

--- trabalho-pratico/src/parsers/aircrafts/parser.c
+++ trabalho-pratico/src/parsers/aircrafts/parser.c
@@ -4,3 +4,10 @@
 #include "managers/aircrafts/structure_aircrafts.h"
 
 // Carrega uma linha do dataset das aeronaves
+int load_line_aircrafts (char *input, Database database) {
+
+    // Carrega o identificador
+    char *identifier = separate_block (&input, '"');
+
+    // Carrega o fabricante
+    char *manufacturer = separate_block (&input, '"');
