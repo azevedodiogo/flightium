ed6679edb95a98761504c595f77b7492312ba82d
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-29 14:20:23 +0000

    feat: initialize scaffolding for reservation parser (parser.c)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

---
+++ trabalho-pratico/src/parsers/reservations/parser.c
@@ -0,0 +1,8 @@
+#include "parsers/parser_reservations.h"
+
+#include "utils/utility_functions.h"
+#include "managers/airports/structure_airports.h"
+#include "managers/flights/entity_flight.h"
+#include "managers/flights/structure_flights.h"
+#include "managers/passengers/entity_passenger.h"
+#include "managers/passengers/structure_passengers.h"
