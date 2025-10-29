65d0c10f17dffe791b97abec72eb4dbda47d2f36
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-29 17:22:33 +0000

    feat: initialize scaffolding for reservations manager (structure_reservations.h)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/include/managers/reservations/structure_reservations.h
@@ -0,0 +1,6 @@
+#ifndef STRUCTURE_RESERVATIONS_H
+#define STRUCTURE_RESERVATIONS_H
+
+/** @brief Estrutura de armazenamento das reservas. */
+typedef struct storage_reservations *StorageReservations;
+
