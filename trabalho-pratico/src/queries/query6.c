02c18117958bc373ee1fdf33265864d7b855b489
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-29 22:26:08 +0000

    refactor: optimize structures in query6.c for query 6
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/src/queries/query6.c
+++ trabalho-pratico/src/queries/query6.c
@@ -5,3 +5,9 @@
 
 #define NO_VALUE -1
 #define LARGE_BUFFER 4096
+
+// Executa a query 6
+void execute_query6 (Database database, char *input, char *output, char del) {
+
+    // Número de visitas do aeroporto
+    int arrival_count = NO_VALUE;
