d8ab3786b7d2e874dd30365b4c6cdae5c2b380e6
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-29 22:38:21 +0000

    feat: add helper functions and routines to structure_passengers.h
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/include/managers/passengers/structure_passengers.h
+++ trabalho-pratico/include/managers/passengers/structure_passengers.h
@@ -13,3 +13,10 @@ typedef struct entity_passenger *EntityPassenger;
 // Há abstração das estruturas internas através de typedefs para ponteiros.
 // Desta maneira, se houvesse necessidade de alterar alguma estrutura para outra, bastava mudar o ficheiro structure.c.
 // O resto do programa continuaria a chamar a função get_passenger, sem saber que por trás aconteceu a troca de uma Hash Table para um Array. 
+
+/**
+ * @brief Cria a estrutura de armazenamento dos passageiros.
+ * @param inicial_capacity Capacidade inicial pretendida.
+ * @return Estrutura inicializada.
+ */
+StoragePassengers create_storage_passengers (int inicial_capacity);
