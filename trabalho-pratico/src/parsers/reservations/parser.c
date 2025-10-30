0f1e6beb7464ab7a13b207076f1fad812b42a5e8
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-30 09:32:02 +0000

    refactor: optimize structures in parser.c for reservation parser

--- trabalho-pratico/src/parsers/reservations/parser.c
+++ trabalho-pratico/src/parsers/reservations/parser.c
@@ -6,3 +6,11 @@
 #include "managers/flights/structure_flights.h"
 #include "managers/passengers/entity_passenger.h"
 #include "managers/passengers/structure_passengers.h"
+#include "managers/reservations/structure_reservations.h"
+#include "parsers/general_validations.h"
+
+#include <math.h>
+
+#define NO_VALUE -1
+
+// Verifica se uma string representa corretamente uma lista de ids de voos
