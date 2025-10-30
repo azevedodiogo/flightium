e622d3519a4daa0540ccfd1ec4fdd337ddff17ad
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-30 17:32:04 +0000

    feat: initialize scaffolding for flights manager (entity_flight.h)

---
+++ trabalho-pratico/include/managers/flights/entity_flight.h
@@ -0,0 +1,8 @@
+#ifndef ENTITY_FLIGHT_H
+#define ENTITY_FLIGHT_H
+
+#include "utils/date_functions.h"
+
+#include <glib.h>
+
+/** @brief Entidade que representa um voo agregado por semana e estado. */
