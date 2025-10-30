a5b9044c018590e417674b06fde06fd0cafb3a95
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-30 15:40:02 +0000

    feat: complete implementation of reservation parser in validations.c
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/src/parsers/reservations/validations.c
+++ trabalho-pratico/src/parsers/reservations/validations.c
@@ -10,3 +10,10 @@ int validate_reservation_id (const char *string) {
 
     // Verifica se o primeiro caractere não é um R
     if (string [0] != 'R') return NO_VALUE;
+
+    // Verifica se os caracteres seguintes são dígitos
+    int i; for (i = 1; i < 10; i++) if (!isdigit (string [i])) return NO_VALUE;
+
+    // Verifica se a string terminou
+    return string [i] == 0 ? atoi (string + 1) : NO_VALUE;
+}
