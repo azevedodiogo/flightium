7d5c3b93af76bf233ec139c2a4b892497ac8d954
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-31 10:03:44 +0000

    refactor: clean up and simplify codebase in entity.c
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/src/managers/passengers/entity.c
+++ trabalho-pratico/src/managers/passengers/entity.c
@@ -13,3 +13,11 @@ typedef struct entity_passenger {
 
 // Cria a informação sobre um passageiro
 EntityPassenger create_passenger (const char *first_name, const char *last_name, const char *nationality, int document_number, date dob) {
+
+    // Aloca memória para o registo
+    EntityPassenger passenger = g_malloc (sizeof (ENTITY_PASSENGER));
+
+    // Define as componentes do registo
+    passenger -> first_name = g_strdup (first_name);
+    passenger -> last_name = g_strdup (last_name);
+    passenger -> nationality = g_strdup (nationality);
