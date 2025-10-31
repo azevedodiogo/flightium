c8a3755d28417368eb218bea3a8ec81adfc16b37
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-10-31 16:47:29 +0000

    refactor: optimize structures in entity.c for airports manager
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/src/managers/airports/entity.c
+++ trabalho-pratico/src/managers/airports/entity.c
@@ -5,3 +5,11 @@
 #define SIZE_COUNTRY 2
 
 // Entidade do aeroporto
+typedef struct entity_airport {
+	char *code;
+	char *name;
+	char *city;
+	char *type;
+	unsigned short arrival_count;
+	unsigned short departure_count;
+	char country [SIZE_COUNTRY + 1];
