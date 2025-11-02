6e2125286736fc276ca6d89c16ea7c79b049f118
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-02 19:42:57 +0000

    refactor: clean up and simplify codebase in entity_aircraft.h
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/include/managers/aircrafts/entity_aircraft.h
+++ trabalho-pratico/include/managers/aircrafts/entity_aircraft.h
@@ -5,3 +5,11 @@
 
 /**
  * @brief Entidade que representa uma aeronave identificada pelo seu código.
+ */
+typedef struct entity_aircraft *EntityAircraft;
+
+
+
+/**
+ * @brief Cria um registo de aeronave.
+ * @param identifier Código único da aeronave.
