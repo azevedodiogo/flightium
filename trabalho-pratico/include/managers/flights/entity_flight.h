a1567c8219ab90626f094bc75d023b1c8e7d4afb
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-02 14:43:28 +0000

    feat: add helper functions and routines to entity_flight.h
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/include/managers/flights/entity_flight.h
+++ trabalho-pratico/include/managers/flights/entity_flight.h
@@ -6,3 +6,11 @@
 #include <glib.h>
 
 /** @brief Entidade que representa um voo agregado por semana e estado. */
+typedef struct entity_flight *EntityFlight;
+
+
+
+/**
+ * @brief Cria um registo de voo.
+ * @param origin Código do aeroporto de origem.
+ * @param destination Código do aeroporto de destino.
