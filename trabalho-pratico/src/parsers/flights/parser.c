5c5da41e2380837ccb13e604fc4da8895c1b9673
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-03 11:59:43 +0000

    feat: initialize scaffolding for flight parser (parser.c)

---
+++ trabalho-pratico/src/parsers/flights/parser.c
@@ -0,0 +1,7 @@
+#include "parsers/parser_flights.h"
+
+#include "utils/utility_functions.h"
+#include "managers/aircrafts/entity_aircraft.h"
+#include "managers/aircrafts/structure_aircrafts.h"
+#include "managers/airlines/structure_airlines.h"
+#include "managers/airports/structure_airports.h"
