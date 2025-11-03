da9c86de9d512b5bcd03b2b0e311cb14ae0a1bed
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-03 11:51:05 +0000

    feat: initialize scaffolding for tests (tests_utils.c)

---
+++ trabalho-pratico/src/tests/test_utils/tests_utils.c
@@ -0,0 +1,7 @@
+#include "tests/tests_utils.h"
+
+#include "tests/test_results/load_timer.h"
+#include "tests/test_results/query_errors.h"
+#include "tests/test_results/query_results.h"
+
+#include <stdio.h>
