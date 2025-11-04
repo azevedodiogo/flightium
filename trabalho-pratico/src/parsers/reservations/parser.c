093521a20da9a02672f59297394fcb803a3c2a0e
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-04 13:05:29 +0000

    refactor: clean up and simplify codebase in parser.c

--- trabalho-pratico/src/parsers/reservations/parser.c
+++ trabalho-pratico/src/parsers/reservations/parser.c
@@ -14,3 +14,10 @@
 #define NO_VALUE -1
 
 // Verifica se uma string representa corretamente uma lista de ids de voos
+static int validate_reservation_flights (char *string, gconstpointer *flight_outbound, gconstpointer *flight_return, gpointer storage_flights) {
+
+    // Verifica se o primeiro caractere do bloco não é o início da lista - '['
+    if (string [0] != '[') return EXIT_FAILURE;
+
+    // Carrega o primeiro id de voo
+    *flight_outbound = get_flight_by_id (storage_flights, separate_block (&string, 39));
