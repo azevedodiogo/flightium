383c9ea369e449bdc24598f7c59cf8c862d6261c
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-04 17:33:07 +0000

    refactor: optimize structures in entity.c for airlines manager

--- trabalho-pratico/src/managers/airlines/entity.c
+++ trabalho-pratico/src/managers/airlines/entity.c
@@ -5,3 +5,11 @@
 // Entidade da companhia aérea
 typedef struct entity_airline {
     char *name;
+    int delay_count;
+    int total_delay;
+} ENTITY_AIRLINE, *EntityAirline;
+
+
+
+// Cria um registo da entidade companhia aérea
+EntityAirline create_airline (const char *name, int delay) {
