bc7c14a1d67b91f0f4aa416d520fcd482e15ef88
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-04 15:24:16 +0000

    refactor: optimize structures in date_functions.c for utils

--- trabalho-pratico/src/utils/date_functions.c
+++ trabalho-pratico/src/utils/date_functions.c
@@ -5,3 +5,11 @@
 
 // Número de dias de cada mês
 static const int days_in_month [] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+// Verifica se o ano é bissexto
+static int is_leap_year (int year) {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+// Calcula o número de dias de um mês
+static int n_days_in_month (int year, int month) {
