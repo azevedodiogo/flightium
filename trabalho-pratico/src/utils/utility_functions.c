5c04025e6928ce94edec43102824382b09666ea3
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-04 09:21:35 +0000

    style: format code structure and variables in utility_functions.c
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/src/utils/utility_functions.c
+++ trabalho-pratico/src/utils/utility_functions.c
@@ -4,3 +4,12 @@
 
 #define NO_VALUE -1
 #define BUFFER 1024
+
+// Separa uma string por um caractere
+char *our_strsep (char **str, char stop_character) {
+
+    // Guarda o endereço inicial
+    char *r = *str;
+
+    // Avança na string até ao caractere de paragem ou ao fim da string
+    while ((*str) [0] && (*str) [0] != stop_character) (*str)++;
