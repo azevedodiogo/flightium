3dcbd1b497d27af7df01f731a5f725378ca9ccff
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-05 22:59:43 +0000

    refactor: optimize structures in parser.c for flight parser

--- trabalho-pratico/src/parsers/flights/parser.c
+++ trabalho-pratico/src/parsers/flights/parser.c
@@ -5,3 +5,10 @@
 #include "managers/aircrafts/structure_aircrafts.h"
 #include "managers/airlines/structure_airlines.h"
 #include "managers/airports/structure_airports.h"
+#include "managers/flights/structure_flights.h"
+#include "parsers/general_validations.h"
+
+#define NO_VALUE -1
+
+// Carrega uma linha do dataset dos voos
+int load_line_flights (char *input, Database database) {
