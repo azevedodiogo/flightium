4e7c5e46517e8f89c6332dd7d5e3a9f509552345
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-05 21:24:28 +0000

    docs: add detailed comments and docstrings in database.h
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/include/managers/database.h
+++ trabalho-pratico/include/managers/database.h
@@ -5,3 +5,12 @@
  * @brief Estrutura opaca que agrega todas as entidades do sistema.
  */
 typedef struct database *Database;
+
+/** @brief Estrutura de armazenamento das aeronaves. */
+typedef struct storage_aircrafts *StorageAircrafts;
+
+/** @brief Estrutura de armazenamento das companhias aéreas. */
+typedef struct storage_airlines *StorageAirlines;
+
+/** @brief Estrutura de armazenamento dos aeroportos. */
+typedef struct storage_airports *StorageAirports;
