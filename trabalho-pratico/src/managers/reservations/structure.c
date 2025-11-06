2c3e6a4b511ef4633ac84fdd415bd80c340b5de3
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-06 14:38:31 +0000

    style: format code structure and variables in structure.c

--- trabalho-pratico/src/managers/reservations/structure.c
+++ trabalho-pratico/src/managers/reservations/structure.c
@@ -6,3 +6,11 @@
 typedef struct storage_reservations {
     GHashTable *data;
 } STORAGE_RESERVATIONS, *StorageReservations;
+
+
+
+// Cria a estrutura de armazenamento da entidade reserva
+StorageReservations create_storage_reservations () {
+
+    // Aloca memória para a estrutura
+    StorageReservations storage_reservations = g_malloc (sizeof (STORAGE_RESERVATIONS));
