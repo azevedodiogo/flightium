f1cd562d2abe916f67b6c6646075356b90bd1a10
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-07 22:05:03 +0000

    feat: initialize scaffolding for aircrafts manager (structure_aircrafts.h)
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

---
+++ trabalho-pratico/include/managers/aircrafts/structure_aircrafts.h
@@ -0,0 +1,8 @@
+#ifndef STRUCTURE_AIRCRAFTS_H
+#define STRUCTURE_AIRCRAFTS_H
+
+/** @brief Estrutura de armazenamento das aeronaves. */
+typedef struct storage_aircrafts *StorageAircrafts;
+
+/** @brief Entidade da aeronave. */
+typedef struct entity_aircraft *EntityAircraft;
