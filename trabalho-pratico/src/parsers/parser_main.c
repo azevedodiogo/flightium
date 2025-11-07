0a95918422f744eed756a34120e2df8b5045d066
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-07 11:47:01 +0000

    feat: improve validation and handle bounds in parser_main.c

--- trabalho-pratico/src/parsers/parser_main.c
+++ trabalho-pratico/src/parsers/parser_main.c
@@ -6,3 +6,11 @@
 #define BUFFER 1024
 
 // Funções de carregamento dos datasets
+static int (*load_line [N_DATASETS]) (char *, Database) = {load_line_aircrafts, load_line_airports, load_line_passengers, load_line_flights, load_line_reservations};
+
+
+
+// Abre os ficheiros dos datasets
+int open_dataset_files (FILE *read_files [], FILE *write_files [], const char *directory) {
+
+    // Declara os nomes dos ficheiros de leitura
