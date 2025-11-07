6d9d39ec51ece753dcb88bfdd635321c1462c7df
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-07 10:00:14 +0000

    feat: initialize scaffolding for interactive mode (input_queries.h)

---
+++ trabalho-pratico/include/interactive_program/input_queries.h
@@ -0,0 +1,7 @@
+#ifndef INPUT_QUERIES
+#define INPUT_QUERIES
+
+/**
+ * @brief Direciona a recolha de argumentos para a query selecionada.
+ * @param query_number Número identificador da query (1 a 6).
+ * @param input Área onde é devolvido o texto formatado com os argumentos.
