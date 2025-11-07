73154e804c1bd0b6d083780686b2559a56acb4cf
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-07 15:47:30 +0000

    feat: add helper functions and routines to check_results.c
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/src/tests/test_utils/check_results.c
+++ trabalho-pratico/src/tests/test_utils/check_results.c
@@ -6,3 +6,10 @@
 #include <stdio.h>
 
 #define NO_VALUE -1
+#define TEST_ERROR 1
+#define TEST_CORRECT 2
+#define TEST_INCORRECT 3
+#define BUFFER 1024
+
+// Extrai uma linha específica do conteúdo de um ficheiro
+static void extract_line_from_content (const char *content, int line_number, char *buffer) {
