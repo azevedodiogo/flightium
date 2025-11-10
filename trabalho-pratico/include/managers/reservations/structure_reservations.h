79fb1c759479289789143bec4383e4852fc83592
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-10 22:13:52 +0000

    refactor: optimize structures in structure_reservations.h for reservations manager

--- trabalho-pratico/include/managers/reservations/structure_reservations.h
+++ trabalho-pratico/include/managers/reservations/structure_reservations.h
@@ -4,3 +4,10 @@
 /** @brief Estrutura de armazenamento das reservas. */
 typedef struct storage_reservations *StorageReservations;
 
+
+
+/**
+ * @brief Cria a estrutura de armazenamento das reservas.
+ * @return Estrutura inicializada.
+ */
+StorageReservations create_storage_reservations ();
