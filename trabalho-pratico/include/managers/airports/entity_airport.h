1cd38c45c6f7f4f7a92db89027dc99c958d19262
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-10 20:54:28 +0000

    refactor: optimize structures in entity_airport.h for airports manager
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/include/managers/airports/entity_airport.h
+++ trabalho-pratico/include/managers/airports/entity_airport.h
@@ -5,3 +5,12 @@
 
 /** @brief Entidade que representa um aeroporto. */
 typedef struct entity_airport *EntityAirport;
+
+
+
+/**
+ * @brief Cria um registo de aeroporto.
+ * @param code Código IATA/ICAO do aeroporto.
+ * @param name Nome completo.
+ * @param city Cidade onde se insere.
+ * @param type Tipo de aeroporto. Tipo pode ser "large_airport", "medium_airport", "small_airport", etc.
