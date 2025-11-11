17def2033f2e931b3a74928145268052b90e388e
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-11 22:05:46 +0000

    feat: complete implementation of query 6 in query6.c

--- trabalho-pratico/src/queries/query6.c
+++ trabalho-pratico/src/queries/query6.c
@@ -11,3 +11,11 @@ void execute_query6 (Database database, char *input, char *output, char del) {
 
     // Número de visitas do aeroporto
     int arrival_count = NO_VALUE;
+
+    // Determina o aeroporto mais visitado para passageiros da nacionalidade dada
+    const struct entity_airport *airport = most_visited_airport_by_nationality (get_database_airports (database), input, &arrival_count);
+
+    // Define o output
+    if (airport) g_snprintf (output, LARGE_BUFFER, "%s%c%d\n", get_airport_code (airport), del, arrival_count);
+    else *output = '\n';
+}
