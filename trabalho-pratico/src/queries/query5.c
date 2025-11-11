49da242e08b524d2e5c9dd65f34d823fa9b7c87a
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-11 09:58:36 +0000

    refactor: optimize structures in query5.c for query 5

--- trabalho-pratico/src/queries/query5.c
+++ trabalho-pratico/src/queries/query5.c
@@ -5,3 +5,12 @@
 #include "managers/storage_generic.h"
 
 #define LARGE_BUFFER 4096
+
+// Executa a query 5
+void execute_query5 (Database database, char *input, char *output, char del) {
+
+    // Determina o argumento do input
+    int N = atoi (input);
+
+    // Declara o array de companhias aéreas encontradas
+    const struct entity_airline *found [N];
