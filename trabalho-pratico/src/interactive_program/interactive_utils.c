344502ce6ca4cfc4bd414f5e10911e5e93a793df
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-12 18:16:58 +0000

    style: format code structure and variables in interactive_utils.c
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/src/interactive_program/interactive_utils.c
+++ trabalho-pratico/src/interactive_program/interactive_utils.c
@@ -6,3 +6,10 @@
 
 #include <stdlib.h>
 #include <string.h>
+
+#define DEFAULT_PATH1 "../dataset-fase-1/com_erros/"
+#define DEFAULT_PATH2 "../dataset-fase-2/com_erros/"
+#define ERROR -1
+#define NUM_QUERIES 6
+#define BUFFER 1024
+#define LARGE_BUFFER 4096
