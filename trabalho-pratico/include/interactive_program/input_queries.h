43bb4e52e6d2cc06ef5a64a2976524eb6ff97bca
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-13 14:00:13 +0000

    refactor: optimize structures in input_queries.h for interactive mode
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/include/interactive_program/input_queries.h
+++ trabalho-pratico/include/interactive_program/input_queries.h
@@ -13,3 +13,11 @@ int get_query_arguments (int query_number, char *input, int max_y, int max_x);
 
 /**
  * @brief Solicita o código do aeroporto para a query 1.
+ * @param max_y Altura disponível do ecrã.
+ * @param max_x Largura disponível do ecrã.
+ * @param input Área de memória onde é colocado o argumento recolhido.
+ * @return EXIT_SUCCESS se a validação for concluída. EXIT_FAILURE se o utilizador cancelar.
+ */
+int get_query1_arguments (int max_y, int max_x, char *input);
+
+/**
