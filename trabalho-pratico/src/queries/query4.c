7af3f6d11b58e3975b73c104d3b6bd2cbf1be0fd
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-17 12:58:05 +0000

    feat: initialize scaffolding for query 4 (query4.c)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

---
+++ trabalho-pratico/src/queries/query4.c
@@ -0,0 +1,7 @@
+#include "queries/queries.h"
+
+#include "utils/date_functions.h"
+#include "managers/passengers/entity_passenger.h"
+#include "managers/passengers/structure_passengers.h"
+
+#define NO_VALUE -1
