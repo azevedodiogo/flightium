e5b77c5007bd7f5089da7333cb6cb7e326df3f18
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-19 22:52:48 +0000

    style: format code structure and variables in database.c

--- trabalho-pratico/src/managers/database.c
+++ trabalho-pratico/src/managers/database.c
@@ -14,3 +14,12 @@ typedef struct database {
     StorageAircrafts aircrafts;
     StorageAirlines airlines;
     StorageAirports airports;
+    StorageFlights flights;
+    StoragePassengers passengers;
+    StorageReservations reservations;
+} DATABASE, *Database;
+
+// Tamanhos dos arrays
+static const int size_aircrafts [] = {1000, 5000};
+static const int size_airlines [] = {30, 30};
+static const int size_airports [] = {7355, 7355};
