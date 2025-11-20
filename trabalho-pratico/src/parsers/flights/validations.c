cd9c8267c8b3fac3acc10a64bb1afba9e594c3f7
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-20 09:23:28 +0000

    feat: improve validation and handle bounds in validations.c
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/src/parsers/flights/validations.c
+++ trabalho-pratico/src/parsers/flights/validations.c
@@ -3,3 +3,11 @@
 #include "utils/date_functions.h"
 
 #include <glib.h>
+
+// Verifica se uma string representa corretamente 'N/A'
+int validate_not_applicable (const char *string) {
+    return strcmp (string, "N/A") == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+// Verifica se as datas de um voo são válidas
+int validate_flight_dates (const char status, date departure, date actual_departure, date arrival, date actual_arrival) {
