f4fbc31d58cce71754c5e68bef0120ad5761002e
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-21 18:27:17 +0000

    feat: improve validation and handle bounds in structure.c
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/src/managers/flights/structure.c
+++ trabalho-pratico/src/managers/flights/structure.c
@@ -20,3 +20,10 @@ StorageFlights create_storage_flights (int inicial_capacity) {
     storage_flights -> data = create_storage_generic (inicial_capacity, destroy_flight, 1);
 
     // Retorna a estrutura
+    return storage_flights;
+}
+
+// Elimina a estrutura de armazenamento dos voos
+void destroy_storage_flights (StorageFlights storage_flights) {
+
+    // Elimina a sub-estrutura
