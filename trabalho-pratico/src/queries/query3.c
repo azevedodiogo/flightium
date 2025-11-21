1c9e7c8dbc2fdb3cfe852036e651ee7f408fd1e3
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-21 13:30:14 +0000

    docs: add detailed comments and docstrings in query3.c
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

--- trabalho-pratico/src/queries/query3.c
+++ trabalho-pratico/src/queries/query3.c
@@ -21,3 +21,10 @@ static void determine_arguments_query3 (char *input, int min_date [3], int max_d
 void execute_query3 (Database database, char *input, char *output, char del) {
 
     // Determina os argumentos do input
+    int min_date [3], max_date [3], max_count; determine_arguments_query3 (input, min_date, max_date);
+
+    // Converte as datas
+    date min = convert_date (min_date [0], min_date [1], min_date [2]);
+    date max = convert_date (max_date [0], max_date [1], max_date [2]);
+
+    // Determina o aeroporto com mais visitas durante o intervalo de tempo
