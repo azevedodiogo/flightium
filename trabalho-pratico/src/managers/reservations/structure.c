c59970c93196ebfcc763341afeb58ac989004b61
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-21 12:48:41 +0000

    test: update test cases and check bounds in structure.c

--- trabalho-pratico/src/managers/reservations/structure.c
+++ trabalho-pratico/src/managers/reservations/structure.c
@@ -14,3 +14,12 @@ StorageReservations create_storage_reservations () {
 
     // Aloca memória para a estrutura
     StorageReservations storage_reservations = g_malloc (sizeof (STORAGE_RESERVATIONS));
+
+    // Cria a sub-estrutura
+    storage_reservations -> data = g_hash_table_new_full (g_direct_hash, g_direct_equal, NULL, NULL);
+
+    // Retorna a estrutura
+    return storage_reservations;
+}
+
+// Elimina a estrutura de armazenamento da entidade reserva
