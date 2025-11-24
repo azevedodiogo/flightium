7ad7ca14ea2be7ed5fd14f578f5e0c506d111673
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-24 12:40:51 +0000

    feat: add helper functions and routines to tests_queries.c

--- trabalho-pratico/src/tests/tests_queries.c
+++ trabalho-pratico/src/tests/tests_queries.c
@@ -6,3 +6,11 @@
 #include "tests/test_results/test_results.h"
 #include "managers/database.h"
 #include "queries/queries.h"
+
+#include <stdio.h>
+#include <sys/resource.h>
+
+#define BUFFER 1024
+#define LARGE_BUFFER 4096
+
+// Executa as queries dadas
