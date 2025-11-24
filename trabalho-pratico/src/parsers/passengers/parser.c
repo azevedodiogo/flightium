38a698323ebaffd0eb1dff04aa46e4d48b5de83c
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-24 22:17:01 +0000

    feat: initialize scaffolding for passenger parser (parser.c)
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>
    Co-authored-by: veralmeida23 <2310veraalmeida@gmail.com>

---
+++ trabalho-pratico/src/parsers/passengers/parser.c
@@ -0,0 +1,8 @@
+#include "parsers/parser_passengers.h"
+
+#include "utils/utility_functions.h"
+#include "utils/date_functions.h"
+#include "parsers/general_validations.h"
+#include "managers/passengers/structure_passengers.h"
+
+#define NO_VALUE -1
