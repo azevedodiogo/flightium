256d231fc3b8ded01ad44609bd392006b43e09d9
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-25 20:51:34 +0000

    refactor: clean up and simplify codebase in queries.h
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

--- trabalho-pratico/include/queries/queries.h
+++ trabalho-pratico/include/queries/queries.h
@@ -4,3 +4,12 @@
 #include "managers/database.h"
 
 #include <stdio.h>
+
+/**
+ * @brief Abre o ficheiro de entrada que contém as queries a executar.
+ * @param read_file Ponteiro onde é devolvido o identificador do ficheiro aberto.
+ * @param read_file_name Caminho completo para o ficheiro de input.
+ * @return EXIT_SUCCESS em caso de abertura bem-sucedida. EXIT_FAILURE em caso contrário.
+ * @warning O ficheiro permanece aberto após o retorno.  O chamador assume a responsabilidade por o fechar.
+ */
+int open_input_file (FILE **read_file, const char *read_file_name);
