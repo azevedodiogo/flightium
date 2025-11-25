980db1c9299645a3811e8e19944a8970e4835ef2
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-25 22:33:24 +0000

    refactor: clean up and simplify codebase in query5.c
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

--- trabalho-pratico/src/queries/query5.c
+++ trabalho-pratico/src/queries/query5.c
@@ -14,3 +14,11 @@ void execute_query5 (Database database, char *input, char *output, char del) {
 
     // Declara o array de companhias aéreas encontradas
     const struct entity_airline *found [N];
+
+    // Determina as N companhias aéreas com maior tempo de atraso médio
+    int number_found = most_delayed_airlines (get_database_airlines (database), N, found);
+
+    // Percorre o array até encontrar todas as aeronaves pretendidas
+    for (int i = 0; i < number_found; i++) {
+
+        // Determina o tamanho do output
