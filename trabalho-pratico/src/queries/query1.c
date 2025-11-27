bffe25d1a98e322c1063d03c60a3b594cf01b2f2
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-11-27 13:34:29 +0000

    feat: complete implementation of query 1 in query1.c
    Co-authored-by: Brumag777 <a110274@alunos.uminho.pt>

--- trabalho-pratico/src/queries/query1.c
+++ trabalho-pratico/src/queries/query1.c
@@ -19,3 +19,11 @@ void execute_query1 (Database database, char *input, char *output, char del) {
         const char *city = get_airport_city (airport);
         const char *country = get_airport_country (airport);
         const char *type = get_airport_type (airport);
+        int arrival_count = get_airport_arrival_count (airport);
+        int departure_count = get_airport_departure_count (airport);
+
+        // Define o output
+        g_snprintf (output, LARGE_BUFFER, "%s%c%s%c%s%c%s%c%s%c%d%c%d\n", input, del, name, del, city, del, country, del, type, del, arrival_count, del, departure_count);
+    
+    } else *output = '\n';
+}
