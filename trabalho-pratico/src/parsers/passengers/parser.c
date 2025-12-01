d84925e58d4d298b4b695f4109b9748434b3a80e
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-12-01 18:58:54 +0000

    refactor: clean up and simplify codebase in parser.c

--- trabalho-pratico/src/parsers/passengers/parser.c
+++ trabalho-pratico/src/parsers/passengers/parser.c
@@ -13,3 +13,12 @@ int load_line_passengers (char *input, Database database) {
     // Carrega o número de documento
     int document_number = validate_document_number (separate_block (&input, '"'));
     if (document_number == NO_VALUE) return EXIT_FAILURE;
+
+    // Carrega o primeiro nome
+    char *first_name = separate_block (&input, '"');
+
+    // Carrega o último nome
+    char *last_Name = separate_block (&input, '"');
+
+    // Carrega a data de nascimento
+    date dob = validate_date (separate_block (&input, '"'), 0);
