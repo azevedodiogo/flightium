0de98095387fa6c0b344d1fdff82e7c985e5a2a3
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-12-02 12:50:48 +0000

    refactor: optimize structures in query_results.h for tests

--- trabalho-pratico/include/tests/test_results/query_results.h
+++ trabalho-pratico/include/tests/test_results/query_results.h
@@ -14,3 +14,11 @@ typedef struct storage_query_results *StorageQueryResults;
 /**
  * @brief Cria um registo de resultado de query.
  * @return Nova entidade resultado ou NULL em caso de falha.
+ */
+QueryResult create_query_result ();
+
+/**
+ * @brief Liberta a memória associada a um resultado de query.
+ * @param query_result Entidade a destruir.
+ */
+void destroy_query_result (QueryResult query_result);
