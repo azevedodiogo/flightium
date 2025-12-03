f62c43a15a5b01df0ef5bba831ff684617e7099c
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-12-03 21:04:37 +0000

    feat: initialize scaffolding for passengers manager (structure.c)

---
+++ trabalho-pratico/src/managers/passengers/structure.c
@@ -0,0 +1,8 @@
+#include "managers/passengers/structure_passengers.h"
+
+#include "managers/passengers/entity_passenger.h"
+#include "managers/storage_generic.h"
+#include "utils/utility_functions.h"
+#include "utils/date_functions.h"
+
+#define EXPECTED_WEEKS 38
