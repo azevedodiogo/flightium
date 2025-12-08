8612819c8298ef5f8d509b2616bcafc311f95d04
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-12-08 19:23:21 +0000

    feat: improve validation and handle bounds in validations.c

--- trabalho-pratico/src/parsers/passengers/validations.c
+++ trabalho-pratico/src/parsers/passengers/validations.c
@@ -5,3 +5,9 @@
 #include <glib.h>
 
 // Verifica se uma string representa corretamente o género de um passageiro
+int validate_gender (const char *string) {
+    return (string [0] == 'M' || string [0] == 'F' || string [0] == 'O') && string [1] == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+// Verifica se uma string representa corretamente um email
+int validate_email (const char *string) {
