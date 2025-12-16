3bab67fdb10ff5c70cb508eba659bacf8c1af927
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-12-16 22:53:44 +0000

    test: update test cases and check bounds in structure.c

--- trabalho-pratico/src/managers/airports/structure.c
+++ trabalho-pratico/src/managers/airports/structure.c
@@ -14,3 +14,11 @@ typedef struct storage_airports {
     StorageGeneric matrix_nationalities;
     int **matrix_flights;
     int len_flights, offset_flights;
+} STORAGE_AIRPORTS, *StorageAirports;
+
+
+
+// Cria a estrutura de armazenamento dos aeroportos
+StorageAirports create_storage_airports (int inicial_capacity) {
+
+    // Aloca memória para a estrutura de armazenamento
