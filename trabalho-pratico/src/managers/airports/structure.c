dd46dbd7aec7b52ab9a923634059ec04861c6ecb
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2025-12-19 14:01:35 +0000

    feat: improve validation and handle bounds in structure.c

--- trabalho-pratico/src/managers/airports/structure.c
+++ trabalho-pratico/src/managers/airports/structure.c
@@ -22,3 +22,10 @@ typedef struct storage_airports {
 StorageAirports create_storage_airports (int inicial_capacity) {
 
     // Aloca memória para a estrutura de armazenamento
+    StorageAirports storage_airports = g_malloc (sizeof (STORAGE_AIRPORTS));
+
+    // Cria as sub-estruturas de armazenamento
+    storage_airports -> data = create_storage_generic (inicial_capacity, destroy_airport, 1);
+    storage_airports -> matrix_nationalities = create_storage_generic (N_EXPECTED_NATIONALITIES, g_free, 1);
+    storage_airports -> matrix_flights = g_malloc (N_EXPECTED_DAYS * sizeof (int *));
+    storage_airports -> len_flights = N_EXPECTED_DAYS; storage_airports -> offset_flights = 0;
